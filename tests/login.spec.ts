c7659925ad4f1570ccc6dd743a83156056f876ce
Diana <ivan@Diana-MacBook-Pro.local>
2025-07-21 22:14:12 -0300

    Se agergaron casos backend

--- tests/login.spec.ts
+++ tests/login.spec.ts
@@ -3,9 +3,11 @@ import { RegisterPage } from '../Pages/Registerpage';
 import testData from '../data/testData.json';
 import { DashboardPage } from '../Pages/dashboardPage';
 import { LoginPage } from '../Pages/loginPage';
+import { BackendUtils } from '../utils/backendUtils';
 
 let loginPage: LoginPage;
 let dashboardPage: DashboardPage;
+let backendUtils: BackendUtils;
 
 
 test.beforeEach (async ({page}) => {
@@ -19,6 +21,30 @@ test.beforeEach (async ({page}) => {
 
 test('TC-7 Verificar inicio de sesión exitoso con credenciales válidas', async ({ page }) => {
   await loginPage.completarYHacerClickBotonLogin(testData.usuarioValido);
+  await expect(page.getByText('Inicio de sesión exitoso')).toBeVisible();
+  await expect(dashboardPage.dashboardTitle).toBeVisible();
+});
+
+test('TC-11 Loguearse con nuevo usuario creado por backend', async ({ page, request }) => {
+  const nuevoUsuario = await BackendUtils.crearUsuarioPorAPI(request, testData.usuarioValido);
+
+  const responsePromiseLogin = page.waitForResponse('http://localhost:4000/api/auth/login');
+  await loginPage.completarYHacerClickBotonLogin(nuevoUsuario);
+
+  const responseLogin = await responsePromiseLogin;
+  const responseBodyLoginJson = await responseLogin.json();
+
+  expect(responseLogin.status()).toBe(200);
+  expect(responseBodyLoginJson).toHaveProperty('token');
+  expect(typeof responseBodyLoginJson.token).toBe('string');
+  expect(responseBodyLoginJson).toHaveProperty('user');
+  expect(responseBodyLoginJson.user).toEqual(expect.objectContaining({
+    id: expect.any(String),
+    firstName: testData.usuarioValido.nombre,
+    lastName: testData.usuarioValido.apellido,
+    email: nuevoUsuario.email,
+  }));
+
   await expect(page.getByText('Inicio de sesión exitoso')).toBeVisible();
   await expect(dashboardPage.dashboardTitle).toBeVisible();
 });
--- tests/register.spec.ts
+++ tests/register.spec.ts
@@ -1,13 +1,14 @@
-import { test, expect } from '@playwright/test';
+import { test, expect, request } from '@playwright/test';
 import { RegisterPage } from '../Pages/Registerpage'; 
 import testData from '../data/testData.json';
 import { DashboardPage } from '../Pages/dashboardPage';
 import { LoginPage } from '../Pages/loginPage';
+import { BackendUtils } from '../utils/backendUtils';
 
 let registerPage: RegisterPage;
 let loginPage: LoginPage;
 let dashboardPage: DashboardPage;
-
+let backendUtils: BackendUtils;
 
 test.beforeEach (async ({page}) => {
   registerPage = new RegisterPage(page);
@@ -56,18 +57,72 @@ test('TC-5 Verificar Registro exitoso con datos válidos', async ({ page }) => {
 test('TC-6 Verificar que un usuario no pueda registrarse con un correo electronico existente ', async ({ page }) => {
 
   await test.step('Completar el formulario de registro con datos válidos', async () => {
+    await registerPage.completarYHacerClickBotonRegistro(testData.usuarioValido);
+    await expect(page.getByText('Email already in use')).toBeVisible();
+  });
+  
+});
+
+test('TC-8 Verificar inicio de sesión exitoso con credenciales válidas verificando respuesta de API', async ({ page }) => {
     
     const email = (testData.usuarioValido.email.split('@')[0]) + Date.now().toString() + '@' + testData.usuarioValido.email.split('@')[1];
    
     testData.usuarioValido.email = email;
+  
+    await registerPage.completarFormularioRegistro(testData.usuarioValido);
 
-    await registerPage.completarYHacerClickBotonRegistro(testData.usuarioValido);
-  });
+
+  const responsePromise = page.waitForResponse('http://localhost:4000/api/auth/signup')
+  await registerPage.hacerClickBotonRegistro();
+  const response = await responsePromise;
+  const responseBody = await response.json();
+
+
+  expect(response.status()).toBe(201);
+  expect(responseBody).toHaveProperty('token');
+  expect(responseBody).toHaveProperty('user');
+  expect(typeof responseBody.token).toBe('string');
+
   await expect(page.getByText('Registro exitoso')).toBeVisible();
+
 });
 
-test('TC-7 Verificar inicio de sesión exitoso con credenciales válidas', async ({ page }) => {
-  await loginPage.completarYHacerClickBotonLogin(testData.usuarioValido);
-  await expect(page.getByText('Inicio de sesión exitoso')).toBeVisible();
-  await expect(dashboardPage.dashboardTitle).toBeVisible();
+test('TC-9 Generar signup desde la API', async ({ page, request }) => {
+  const email = (testData.usuarioValido.email.split('@')[0]) + Date.now().toString() + '@' + testData.usuarioValido.email.split('@')[1];
+  const response = await request.post('http://localhost:3000/api/auth/signup', { //para esperar el endpoint
+    headers: {
+      'Accept': 'application/json',
+      'Content-Type': 'application/json',
+    },
+    data: {
+      firstName: testData.usuarioValido.nombre,
+      lastName: testData.usuarioValido.apellido,
+      email: email,
+      password: testData.usuarioValido.contraseña,
+    }
+  });
+  const responseBody = await response.json();
+  expect(response.status()).toBe(201);
+  expect(responseBody).toHaveProperty('token');
+  expect(typeof responseBody.token).toBe('string');
+  expect(responseBody).toHaveProperty('user');
+  expect(responseBody.user).toEqual(expect.objectContaining({
+    id: expect.any(String),
+    firstName: testData.usuarioValido.nombre,
+    lastName: testData.usuarioValido.apellido,
+    email: email,
+  }));
 });
+
+test('TC-10 Verificar comportamiento del front ante un error 500 en el registro', async ({ page }) => {
+  await page.route('**/api/auth/signup', route => {
+    route.fulfill({
+      status: 500,
+      contentType: 'application/json',
+      body: JSON.stringify({ message: 'Error en el registro' }),
+    });
+  });
+await registerPage.completarYHacerClickBotonRegistro(testData.usuarioValido);
+await expect(page.getByText('Error en el registro')).toBeVisible();
+});
+
